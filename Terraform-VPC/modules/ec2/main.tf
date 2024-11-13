ecbb30d683fb47a9be37dccae1a0b3cb75fe7853
harshasrigit <165784972+harshasrigit@users.noreply.github.com>
2024-11-13 13:34:32 +0530

    Update main.tf

--- Terraform-VPC/modules/ec2/main.tf
+++ Terraform-VPC/modules/ec2/main.tf
@@ -4,10 +4,10 @@ resource "aws_instance" "web" {
   instance_type             = "t3.micro"  # Free Tier eligible instance type
   associate_public_ip_address = true  # Associate a public IP with the instance
   vpc_security_group_ids    = [var.sg_id]  # Pass the security group ID
-  subnet_id                 = var.subnets[0,1]  # Pass subnet ID for each instance
-  availability_zone         = data.aws_availability_zones.available.names[0,1]  # Use AZs from data source
+  subnet_id                 = var.subnets[count.index]  # Pass subnet ID for each instance
+  availability_zone         = data.aws_availability_zones.available.names[count.index]  # Use AZs from data source
 
   tags = {
-    Name = var.ec2_names[0,1]  # Set the instance name
+    Name = var.ec2_names[count.index]  # Set the instance name
   }
 }
