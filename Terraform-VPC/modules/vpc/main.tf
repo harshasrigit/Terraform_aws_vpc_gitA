f0494240048c2dc6b187b383d60980a6b86f8c9a
harshasrigit <165784972+harshasrigit@users.noreply.github.com>
2024-11-13 12:52:18 +0530

    Update main.tf

--- Terraform-VPC/modules/vpc/main.tf
+++ Terraform-VPC/modules/vpc/main.tf
@@ -8,3 +8,14 @@ resource "aws_vpc" "my_vpc" {
     }
 }
 
+resource "aws_subnet" "subnets" {
+  count = length(var.subnet_cidr)#2
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = var.subnet_cidr[count.index] #
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+  
+  tags = {
+    Name = var.subnet_names[count.index]
+  }
+}
