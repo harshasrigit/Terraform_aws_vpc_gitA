e8557201878ff643363182f43982f635c765649d
harshasrigit <165784972+harshasrigit@users.noreply.github.com>
2024-11-13 13:24:44 +0530

    Update main.tf

--- Terraform-VPC/modules/ec2/main.tf
+++ Terraform-VPC/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   count                     = 1  # length(var.ec2_names)  # Create instances based on the length of ec2_names
   ami                       = "ami-08eb150f611ca277f"  # Specify the AMI
-  instance_type             = "t2.micro"  # Free Tier eligible instance type
+  instance_type             = "t3.micro"  # Free Tier eligible instance type
   associate_public_ip_address = true  # Associate a public IP with the instance
   vpc_security_group_ids    = [var.sg_id]  # Pass the security group ID
   subnet_id                 = var.subnets[0]  # Pass subnet ID for each instance
